4fb77b54b288ca010f7a0e2f0499f983e9905508
Sergio Artero <sergio@ds2u.com.br>
2022-05-30 18:15:28 -0300

    Testando a utilização do import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into restaurante (nome) values ( 'China In Box' );
+insert into restaurante (nome) values ( 'Si Senor' );
+
+insert into cozinha (nome) values ('Tailandesa');
+insert into cozinha (nome) values ('Indiana');
