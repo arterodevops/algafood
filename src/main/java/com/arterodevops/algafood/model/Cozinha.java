8dbad24c5aeb24d0ce297ab7135c4007d88dd909
Sergio Artero <sergio@ds2u.com.br>
2022-05-30 19:03:22 -0300

    Implementando o Lombok

--- src/main/java/com/arterodevops/algafood/model/Cozinha.java
+++ src/main/java/com/arterodevops/algafood/model/Cozinha.java
@@ -1,16 +1,22 @@
 package com.arterodevops.algafood.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     private String nome;
@@ -19,28 +25,4 @@ public class Cozinha {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return getId().equals(cozinha.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
 }
