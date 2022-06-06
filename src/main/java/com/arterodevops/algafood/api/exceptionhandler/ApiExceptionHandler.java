6462e47fc09786b7740bc19868a2b17c4a134900
Sergio Artero <sergio@ds2u.com.br>
2022-06-06 13:14:28 -0300

    customizando o retorno de exception do ResponseStatus

--- src/main/java/com/arterodevops/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/arterodevops/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,16 +1,22 @@
 package com.arterodevops.algafood.api.exceptionhandler;
 
 import com.arterodevops.algafood.domain.exception.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    WebRequest request;
 
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<?> negocioExceptionHandler(NegocioException e) {
@@ -21,50 +27,46 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(RestauranteNotFoundException.class)
     public ResponseEntity<?> restauranteNotFoundExceptionHandler(RestauranteNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(getAlgafoodException(e));
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 
     }
 
     @ExceptionHandler(CozinhaNotFoundException.class)
     public ResponseEntity<?> cozinhaNotFoundExceptionHandler(CozinhaNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(getAlgafoodException(e));
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 
     }
 
     @ExceptionHandler(CidadeNotFoundException.class)
     public ResponseEntity<?> cidadeNotFoundExceptionHandler(CidadeNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(getAlgafoodException(e));
-
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(EntityAlgafoodNotFoundException.class)
     public ResponseEntity<?> entityAlgafoodNotFoundExceptionHandler(EntityAlgafoodNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(getAlgafoodException(e));
-
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(EntityInUseException.class)
     public ResponseEntity<?> entityInUseExceptionHandler(EntityInUseException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(getAlgafoodException(e));
-
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
     @ExceptionHandler(EstadoNotFoundException.class)
     public ResponseEntity<?> EstadoNotFoundExceptionHandler(EstadoNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(getAlgafoodException(e));
-
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                .body(getAlgafoodException("O tipo de mídia não é aceito."));
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        if (body == null) {
+            body = getAlgafoodException(status.getReasonPhrase());
+        } else if (body instanceof String) {
+            body = getAlgafoodException((String) body);
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 
     private AlgafoodException getAlgafoodException(Exception e) {
