c223ec28afe811e0c0b01351ab033a8362c68440
Sergio Artero <sergio@ds2u.com.br>
2022-06-15 17:54:49 -0300

    Preparando o projeto para testes

--- src/test/java/com/arterodevops/algafood/AlgafoodApplicationTests.java
+++ src/test/java/com/arterodevops/algafood/AlgafoodApplicationTests.java
@@ -1,13 +1,19 @@
 package com.arterodevops.algafood;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class AlgafoodApplicationTests {
+public class AlgafoodApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertFalse(false);
 	}
 
 }
